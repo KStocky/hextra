88a82da62bbab059f23d28c8aab3be4f4ff6f4f2
KStocky <kstocky1@gmail.com>
2025-08-29 15:55:46 +0100

    Modify build.sh to only build one branch

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ echo "Using base URL: $BASE_URL"
 
 # Version configuration - modify these arrays to specify versions to build
 # Format: "ref:display_name" (ref can be tag, branch, or commit hash, display name is what will appear in URL)
-MAIN_VERSION="v0.10.1:latest"
+MAIN_VERSION="ForkGHPages:latest"
 VERSIONS=(
-  "main:latest" # latest version always builds from main
-  "v0.9.6:v0.9"
-  "v0.8.6:v0.8"
+  #"main:latest" # latest version always builds from main
+  #"v0.9.6:v0.9"
+  #"v0.8.6:v0.8"
 )
 
 # Parse main version

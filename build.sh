871e06d827e3d4e8016789809689ff2676ce77de
KStocky <kstocky1@gmail.com>
2025-08-30 16:22:18 +0100

    Modifying build.sh now that i have synced upstream tags, so the build script will work by default now

--- build.sh
+++ build.sh
@@ -8,11 +8,12 @@ echo "Using base URL: $BASE_URL"
 
 # Version configuration - modify these arrays to specify versions to build
 # Format: "ref:display_name" (ref can be tag, branch, or commit hash, display name is what will appear in URL)
-MAIN_VERSION="main-fork:latest"
+MAIN_VERSION="v0.10.1:latest"
 VERSIONS=(
-  #"main:latest" # latest version always builds from main
-  #"v0.9.6:v0.9"
-  #"v0.8.6:v0.8"
+  "main-fork:latest-fork"
+  "main:latest" # latest version always builds from main
+  "v0.9.6:v0.9"
+  "v0.8.6:v0.8"
 )
 
 # Parse main version
